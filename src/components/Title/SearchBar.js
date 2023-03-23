e458b8c9c97061392cafdc2c8176102eaaff09c9
Christian Velez Medina <christian.gerardo_@outlook.es>
2023-03-23 16:35:11 -0600

    Searchbar

--- src/components/Title/SearchBar.js
+++ src/components/Title/SearchBar.js
@@ -1,42 +1,41 @@
 import React, { useState } from 'react'
-import { Link } from "react-router-dom";
 
-
-function SearchBar({placeholder, data}) {
-    const [fillteredData, setFillteredData] = useState([]);
-    const handleFilter = (event) => {
-        const searchWord = event.target.value
-        const newFilter = data.filter((value) => {
-            return value.title.toLowerCase().includes(searchWord.toLowerCase());
-        });
-        if (searchWord === "") {
-            setFillteredData([])
-        }
-        else{
-            setFillteredData(newFilter);
-        }
-        
-    };
-  return (
-    <div className='search'>
-        <div className='searchInputs'>
-      <input type='text' placeholder={placeholder} onChange={handleFilter}/>
-      
-        </div>
-        {fillteredData.length != 0 && (
-        <div className='dataResult'>
-            {fillteredData.slice(0,1).map((value, key) => {
-                return (
-                <a className='dataItem' href={value.link} >
-                    <p>{value.title}</p>
-                    </a>
-                );
-            })}
-        </div>
-        )}
-    </div>
-
-  );
+function SearchBar({ placeholder, data }) {
+   const [fillteredData, setFillteredData] = useState([])
+   const handleFilter = (event) => {
+      const searchWord = event.target.value
+      const newFilter = data.filter((value) => {
+         return value.title.toLowerCase().includes(searchWord.toLowerCase())
+      })
+      if (searchWord === '') {
+         setFillteredData([])
+      } else {
+         setFillteredData(newFilter)
+      }
+   }
+   return (
+      <div className='search'>
+         <div className='searchInputs'>
+            <input
+               type='text'
+               className='border-0'
+               placeholder={placeholder}
+               onChange={handleFilter}
+            />
+         </div>
+         {fillteredData.length != 0 && (
+            <div className='dataResult position-absolute p-2'>
+               {fillteredData.slice(0, 1).map((value, key) => {
+                  return (
+                     <a className='dataItem' href={value.link}>
+                        <p>{value.title}</p>
+                     </a>
+                  )
+               })}
+            </div>
+         )}
+      </div>
+   )
 }
 
-export default SearchBar;
+export default SearchBar
