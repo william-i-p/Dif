155e3aa47194d460f1c170c539b97a84a8d01dbd
Christian Velez <66536867+Christian-Velez@users.noreply.github.com>
2023-11-09 17:06:24 -0600

    ajuste pagina de inicio

--- src/components/Partner.js
+++ src/components/Partner.js
@@ -1,30 +1,70 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 
 class Partner extends Component {
-    render(){
-        var {pClass,pClasst} = this.props;
-        return(
-            <section className={`${pClass}`}>
-                <div className="container">
-                    <div className={`partner_logo_area_four ${pClasst}`}>
-                       {/*<h4 className="f_size_18 f_400 f_p text-center l_height28 mb_70">Market leaders use app to nrich their brand & business.</h4> */} 
-                        <div className="row partner_info">
-                            <div  >
-                                <a href="https://www.itei.org.mx/v4/" target="_blank"><img className='altura' src={require ('../img/home-tracking/itei.jpg')} alt=""/></a>
-                            </div>
-                            <div  data-wow-delay="0.2s">
-                                <a href="https://www.infomexjalisco.org.mx/InfomexJalisco/" target="_blank"><img className='altura' src={require ('../img/home-tracking/infomex.jpg')} alt=""/></a>
-                            </div>
-                            <div  data-wow-delay="0.3s">
-                                <a href="https://snt.org.mx/" target="_blank"><img className='altura' src={require ('../img/home-tracking/sinatra.jpg')} alt=""/></a>
-                            </div>
-                            
-                        </div>
-                    </div>
-                </div>
-            </section>
-        )
-    }
+   render() {
+      var { pClass, pClasst } = this.props
+      return (
+         <section className={`${pClass}`}>
+            <div className='container'>
+               <div className={`partner_logo_area_four ${pClasst}`}>
+                  {/*<h4 className="f_size_18 f_400 f_p text-center l_height28 mb_70">Market leaders use app to nrich their brand & business.</h4> */}
+                  <div className='row partner_info'>
+                     <div>
+                        <a href='https://www.itei.org.mx/v4/' target='_blank'>
+                           <img
+                              className='altura'
+                              src={require('../img/home-tracking/itei.jpg')}
+                              alt=''
+                           />
+                        </a>
+                     </div>
+                     <div data-wow-delay='0.2s'>
+                        <a
+                           href='https://www.infomexjalisco.org.mx/InfomexJalisco/'
+                           target='_blank'
+                           rel='noopener noreferrer'
+                        >
+                           <img
+                              className='altura'
+                              src={require('../img/home-tracking/infomex.jpg')}
+                              alt=''
+                           />
+                        </a>
+                     </div>
+
+                     <div data-wow-delay='0.3s'>
+                        <a
+                           href='https://snt.org.mx/'
+                           target='_blank'
+                           rel='noopener noreferrer'
+                        >
+                           <img
+                              className='altura'
+                              src={require('../img/home-tracking/sinatra.jpg')}
+                              alt=''
+                           />
+                        </a>
+                     </div>
+
+                     <div data-wow-delay='0.4s'>
+                        <a
+                           href='https://consultapublicamx.plataformadetransparencia.org.mx'
+                           target='_blank'
+                           rel='noopener noreferrer'
+                        >
+                           <img
+                              className='altura'
+                              src={require('../img/home-tracking/PNT-logo.png')}
+                              alt=''
+                           />
+                        </a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </section>
+      )
+   }
 }
 
-export default Partner;
+export default Partner
